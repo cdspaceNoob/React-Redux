f522510bc146ef55537fcc809e4c8ba952b53f67
voyager <cdspacenoob@gmail.com>
2023-02-15 16:37:51 +0900

    use hooks and try with class component

--- src/store/index.js
+++ src/store/index.js
@@ -2,13 +2,13 @@ import { createStore } from "redux";
 // const redux = require("redux");
 
 const counterReducer = (state = { counter: 0 }, action) => {
-  if ((action.type = "INCREMENT")) {
+  if (action.type === "INCREMENT") {
     return {
       counter: state.counter + 1,
     };
   }
 
-  if ((action.type = "DECREMENT")) {
+  if (action.type === "DECREMENT") {
     return {
       counter: state.counter - 1,
     };
